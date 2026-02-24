6bd3b2ab5a1a0a99ba8d00bcfd0e7f3d724e61f1
Jimmy Vilén <jimmy@vilen.nu>
2026-02-24 14:51:47 +0100

    chore(nvim): added ftplugin for csharp

---
+++ dot_config/nvim/ftplugin/cs.lua
@@ -0,0 +1,6 @@
+-- Standard indentation settings for C#
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.expandtab = true -- Use spaces instead of tabs
+
